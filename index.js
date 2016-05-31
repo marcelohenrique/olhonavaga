d827d1df28bb0bcd25200003810f025c1bbb6053
marcelo.lima <marcelo.lima@DINFO-34934.trt16.gov.br>
2016-05-31 15:11:03 -0300

    Ajustes no javascript.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 $( document ).ready( function() {
+   var div = $( '<div>' );
+   div.html( 'asdf' );
+   div.css( {
+      position : 'fixed',
+      bottom : '0',
+      width : '100%'
+   } )
+   $( 'body' ).append( div );
 } );
